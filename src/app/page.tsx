190cdf633431c7c476cfea1e99e56d85475c6673
prateekvarma <prtk.varma@gmail.com>
2023-07-16 18:45:19 -0600

    FInishing touches for left sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { BiHomeCircle, BiUser } from 'react-icons/bi';
 import { HiOutlineHashtag } from 'react-icons/hi';
-import { BsBell, BsBookmark } from 'react-icons/bs';
-import { HiEnvelope } from 'react-icons/hi2';
+import { BsBell, BsBookmark, BsTwitter, BsEnvelope } from 'react-icons/bs';
 import Link from 'next/link';
 
 const NAVIGATION_ITEMS = [
+  {
+    title: 'Twitter',
+    icon: BsTwitter,
+  },
   {
     title: 'Home',
     icon: BiHomeCircle,
@@ -19,7 +22,7 @@ const NAVIGATION_ITEMS = [
   },
   {
     title: 'Message',
-    icon: HiEnvelope,
+    icon: BsEnvelope,
   },
   {
     title: 'Bookmarks',
@@ -34,26 +37,32 @@ const NAVIGATION_ITEMS = [
 export default function Home() {
   return (
     <div className='w-full h-full flex justify-center items-center relative bg-black'>
-      <div className='max-w-screen-lg w-full flex relative'>
+      <div className='max-w-screen-xl w-full flex relative'>
         {/* Left section */}
-        <section className='fixed w-72 flex flex-col h-screen'>
-          {NAVIGATION_ITEMS.map((item) => {
-            return (
-              <Link
-                key={item.title}
-                href={`/${item.title.toLowerCase()}`}
-                className='bg-white/50 flex item-center justify-center space-x-2 rounded-3xl p-4'
-              >
-                <div>
-                  <item.icon />
-                </div>
-                <div>{item.title}</div>
-              </Link>
-            );
-          })}
+        <section className='fixed w-[275px] flex flex-col items-stretch h-screen space-y-4'>
+          <div className='flex-col items-stretch h-screen space-y-4 my-4'>
+            {NAVIGATION_ITEMS.map((item) => {
+              return (
+                <Link
+                  key={item.title}
+                  href={`/${item.title.toLowerCase()}`}
+                  className='hover:bg-white/10 text-2xl transition duration-200 flex item-center justify-start w-fit space-x-2 rounded-3xl py-2 px-6'
+                >
+                  <div>
+                    <item.icon />
+                  </div>
+                  {item.title !== 'Twitter' && <div>{item.title}</div>}
+                </Link>
+              );
+            })}
+            <button className='rounded-full m-4 bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200'>
+              Tweet
+            </button>
+          </div>
+          <div>at the bottom</div>
         </section>
-        <main>Home timeline</main>
-        <section>Right section</section>
+        <main></main>
+        <section></section>
       </div>
     </div>
   );
