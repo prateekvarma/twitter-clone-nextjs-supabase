4599ffa13815cadf680f8fa8274f8d44510e2e3a
prateekvarma <prtk.varma@gmail.com>
2023-07-23 11:01:18 -0600

    Initial setup for right sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { BsSearch } from 'react-icons/bs';
 import LeftSidebar from './components/LeftSidebar';
 import MainComponent from './components/MainComponent';
 
@@ -5,10 +6,28 @@ export default function Home() {
   return (
     <div className='w-full h-full flex justify-center items-center relative bg-black'>
       <div className='max-w-screen-xl w-full flex relative'>
-        {/* Left section */}
         <LeftSidebar />
         <MainComponent />
-        <section></section>
+        <section className='absolute border-4 right-0 flex flex-col items-stretch h-screen px-6'>
+          <div>
+            <div className='relative w-full h-full'>
+              <label
+                htmlFor='searchBox'
+                className='absolute top-0 left-0 h-full flex items-center justify-center'
+              >
+                <BsSearch className='w-5 h-5 text-gray-500' />
+              </label>
+              <input
+                id='searchBox'
+                type='text'
+                placeholder='Search Twitter'
+                className='outline-none bg-transparent border-none w-full h-full rounded-xl py-4 px-8'
+              />
+            </div>
+          </div>
+          <div></div>
+          <div></div>
+        </section>
       </div>
     </div>
   );
